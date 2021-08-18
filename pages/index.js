a2dad3838d5dd3c213f6b9f1febad9c4a27935dc
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-08-19 01:20:06 +0530

    chore: cleanup index

--- pages/index.js
+++ pages/index.js
@@ -27,10 +27,6 @@ export default function Home({ allPostsData }) {
 					Python, Docker and Django REST framework. I am currently in the
 					process of learning Kotlin.
 				</p>
-				<p>
-					(This is a sample website - you’ll be building a site like this on{" "}
-					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-				</p>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 				<h2 className={utilStyles.headingLg}>Blog</h2>
