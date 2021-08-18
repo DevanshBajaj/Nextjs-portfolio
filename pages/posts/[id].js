186775c2a4056278a30b8096051cada13ffebfcf
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-08-19 00:39:49 +0530

    feat: add content of the post using remark

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,7 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticProps({ params }) {
-	const postData = getPostData(params.id);
+	const postData = await getPostData(params.id);
 	return {
 		props: {
 			postData,
@@ -27,6 +27,8 @@ export default function Post({ postData }) {
 			{postData.id}
 			<br />
 			{postData.date}
+			<br />
+			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
 		</Layout>
 	);
 }
