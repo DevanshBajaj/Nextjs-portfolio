36d410bc7069fb92384cfc27266692529096e47c
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-08-23 16:48:08 +0530

    feat: add dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import "../styles/global.css";
+import { ThemeProvider } from "next-themes";
 
-export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+	return (
+		<ThemeProvider forcedTheme={Component.theme || undefined}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
+
+export default App;
