8a249ab8ca58ac7954bb50fa20f31dbced93725c
Devansh Bajaj <devansh21db.db@gmail.com>
2021-08-28 21:22:55 +0530

    style: style blog page

---
+++ components/Blog.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const Blog = () => {
+	return <></>;
+};
+
+export default Blog;
