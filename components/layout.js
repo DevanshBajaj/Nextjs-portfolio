3b79ed1c971fab840f30721beaf1b1779885588b
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-09-06 00:27:35 +0530

    style: link preview card

--- components/layout.js
+++ components/layout.js
@@ -13,15 +13,12 @@ const Layout = ({ children, home }) => {
 		<div className={styles.container}>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
-				<meta
-					name="description"
-					content="Learn how to build a personal website using Next.js"
-				/>
+				<meta name="description" content="My Personal Website" />
 				<meta
 					property="og:image"
 					content={`https://og-image.vercel.app/${encodeURI(
 						siteTitle
-					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+					)}.png?theme=dark&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg`}
 				/>
 				<meta name="og:title" content={siteTitle} />
 				<meta name="twitter:card" content="summary_large_image" />
