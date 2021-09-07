07a133acf99f608b7f1e381b2f741a49f53d07b9
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-09-07 17:51:21 +0530

    chore: some styling fixes  and about me changes

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,18 @@ export default function Home({ allPostsData }) {
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={`${utilStyles.paragraphMd} ${utilStyles.paddingLg}`}>
-				<h2 className={utilStyles.heading1Xl}>
+				<h2 className={`${utilStyles.heading1Xl} ${utilStyles.paddingLg}`}>
 					About Me<a>.</a>
 				</h2>
 				<p>
-					I&#39;m proficient in JavaScript and CSS / SASS with experience in
-					frameworks like React and Next.js. I have some experience with Python,
-					Docker and Django REST framework. I also have experience in UI/UX
-					designing, I like exploring different ways to make a design user
-					friendly. I&#39;m comfortable with prototyping, wireframing using
-					tools like Figma and Adobe XD.
+					I&#39;m a Frontend developer based in Ghaziabad, India. I have a
+					passion for building web applications and designing beautiful user
+					interfaces.
+					<br />
+					<br />
+					Recently I&#39;ve been working with technologies like Javascript,
+					React, Next.js and I&#39;m currently learning Node.js. I also love
+					working with tools like Figma for designing.
 				</p>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.paddingLg}`}>
