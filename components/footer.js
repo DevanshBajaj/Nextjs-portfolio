e9221f84ece1dd2142b057908dbebe6d84302b38
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-09-08 01:28:49 +0530

    style: add and  update footer styling

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,24 @@
 import styles from "./footer.module.css";
+import { FaTwitter, FaTelegram } from "react-icons/fa";
 
 const footer = () => {
 	return (
 		<div className={styles.footer}>
-			<a>Designed and built by Devansh Bajaj</a>
+			<a className={styles.footerText}> Designed and built by Devansh Bajaj</a>
+			<a
+				className={styles.footerEmail}
+				href="mailto:devansh21db.db@gmail.com?Subject=Hello"
+			>
+				devansh21db.db@gmail.com
+			</a>
+			<div className={styles.logowrapper}>
+				<a href="https://twitter.com/devansh21db" target="twitter">
+					<FaTwitter />
+				</a>
+				<a href="https://t.me/devanshbajaj" target="dribble">
+					<FaTelegram />
+				</a>
+			</div>
 		</div>
 	);
 };
