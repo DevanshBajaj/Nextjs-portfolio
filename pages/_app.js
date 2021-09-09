b2f5a4c609f8a2fe324cd9271fd603d54c021c6d
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-09-09 16:20:04 +0530

    feat: added Progressbar on top while loading pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
 import { AnimatePresence } from "framer-motion";
 import Footer from "../components/footer";
+import NextNprogress from "nextjs-progressbar";
 
 function App({ Component, pageProps }) {
 	return (
@@ -9,6 +10,14 @@ function App({ Component, pageProps }) {
 			themes={["pink", "red", "blue", "light", "dark"]}
 			forcedTheme={Component.theme || undefined}
 		>
+			<NextNprogress
+				color="var(--theme-accent)"
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={4}
+				showOnShallow={true}
+				options={{ easing: "ease", speed: 500, showSpinner: false }}
+			/>
 			<div className="container">
 				<AnimatePresence
 					initial={false}
