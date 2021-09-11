b011528901b0349891a50c7cf190983929a24e09
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-09-11 18:47:18 +0530

    style: improve 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,10 @@
+import utilStyles from "../styles/utils.module.css";
+import Layout from "../components/layout";
+
 export default function Custom404() {
-	return <h1>404 - Page Not Found</h1>;
+	return (
+		<Layout>
+			<h1 className={utilStyles.notfound}>404 - Page Not Found</h1>
+		</Layout>
+	);
 }
