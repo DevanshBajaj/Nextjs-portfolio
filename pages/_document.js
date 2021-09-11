7aed14fbbc68a9699bac815134adc5b562a2ea36
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-09-11 18:47:58 +0530

    chore: add apple splash screens for PWA

--- pages/_document.js
+++ pages/_document.js
@@ -16,8 +16,8 @@ class MyDocument extends Document {
 						name="apple-mobile-web-app-status-bar-style"
 						content="default"
 					/>
-					<meta name="apple-mobile-web-app-title" content="PWA App" />
-					<meta name="description" content="Best PWA App in the world" />
+					<meta name="apple-mobile-web-app-title" content="Portfolio PWA" />
+					<meta name="description" content="Devansh Bajaj's Portfolio" />
 					<meta name="format-detection" content="telephone=no" />
 					<meta name="mobile-web-app-capable" content="yes" />
 					<meta
@@ -28,21 +28,21 @@ class MyDocument extends Document {
 					<meta name="msapplication-tap-highlight" content="no" />
 					<meta name="theme-color" content="#000000" />
 
-					<link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+					<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
 					<link
 						rel="apple-touch-icon"
 						sizes="152x152"
-						href="/icons/touch-icon-ipad.png"
+						href="/icons/apple-touch-icon-152x152.png"
 					/>
 					<link
 						rel="apple-touch-icon"
 						sizes="180x180"
-						href="/icons/touch-icon-iphone-retina.png"
+						href="/icons/apple-touch-icon-180x180.png"
 					/>
 					<link
 						rel="apple-touch-icon"
 						sizes="167x167"
-						href="/icons/touch-icon-ipad-retina.png"
+						href="/icons/apple-touch-icon-152x152.png"
 					/>
 
 					<link
@@ -66,37 +66,67 @@ class MyDocument extends Document {
 					<link rel="shortcut icon" href="/favicon.ico" />
 
 					<meta name="twitter:card" content="summary" />
-					<meta name="twitter:url" content="https://yourdomain.com" />
-					<meta name="twitter:title" content="PWA App" />
+					<meta
+						name="twitter:url"
+						content="https://nextjs-devanshblog.vercel.app/"
+					/>
+					<meta name="twitter:title" content="Portfolio" />
 					<meta
 						name="twitter:description"
-						content="Best PWA App in the world"
+						content="Devansh Bajaj's Portfolio"
 					/>
 					<meta
 						name="twitter:image"
-						content="https://yourdomain.com/icons/android-chrome-192x192.png"
+						content="https://nextjs-devanshblog.vercel.app/icons/android-chrome-192x192.png"
 					/>
 					<meta name="twitter:creator" content="@DavidWShadow" />
 					<meta property="og:type" content="website" />
 					<meta property="og:title" content="PWA App" />
 					<meta property="og:description" content="Best PWA App in the world" />
 					<meta property="og:site_name" content="PWA App" />
-					<meta property="og:url" content="https://yourdomain.com" />
+					<meta
+						property="og:url"
+						content="https://nextjs-devanshblog.vercel.app"
+					/>
 					<meta
 						property="og:image"
-						content="https://yourdomain.com/icons/apple-touch-icon.png"
+						content="https://nextjs-devanshblog.vercel.app/icons/apple-touch-icon.png"
 					/>
-
-					{/* <!-- apple splash screen images -->
-<!--
-<link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-<link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
---> */}
+					<link
+						rel="apple-touch-startup-image"
+						href="/images/apple_splash_2048.png"
+						sizes="2048x2732"
+					/>
+					<link
+						rel="apple-touch-startup-image"
+						href="/images/apple_splash_1668.png"
+						sizes="1668x2224"
+					/>
+					<link
+						rel="apple-touch-startup-image"
+						href="/images/apple_splash_1536.png"
+						sizes="1536x2048"
+					/>
+					<link
+						rel="apple-touch-startup-image"
+						href="/images/apple_splash_1125.png"
+						sizes="1125x2436"
+					/>
+					<link
+						rel="apple-touch-startup-image"
+						href="/images/apple_splash_1242.png"
+						sizes="1242x2208"
+					/>
+					<link
+						rel="apple-touch-startup-image"
+						href="/images/apple_splash_750.png"
+						sizes="750x1334"
+					/>
+					<link
+						rel="apple-touch-startup-image"
+						href="/images/apple_splash_640.png"
+						sizes="640x1136"
+					/>
 				</Head>
 				<body>
 					<Main />
