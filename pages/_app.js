eac9035a11d412def633ae598d00959ffbdcd23c
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-09-13 18:57:42 +0530

    fix: footer and nav styling

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function App({ Component, pageProps }) {
 			<Head>
 				<meta
 					name="viewport"
-					content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86"
+					content="width=device-width, initial-scale=1, maximum-scale=5.0, minimum-scale=0.86"
 				/>
 			</Head>
 			<NextNprogress
