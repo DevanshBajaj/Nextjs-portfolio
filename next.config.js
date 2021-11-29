8039ed86e6e21eb92eadbe38ddfeae4f38cc5052
Devansh Bajaj <devansh21db.db@gmail.com>
2021-11-29 23:45:13 +0530

    chore: remove pwa until nextjs/pwa issue is fixed

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
-	pwa: {
-		dest: "public",
-		disable: process.env.NODE_ENV === "development",
-		register: true,
-		skipWaiting: true,
-		runtimeCaching,
-		buildExcludes: [/middleware-manifest.json$/]
-	},
-	swcMinify: true,
-	images: {
-		formats: ["image/avif", "image/webp"],
-	},
-});
+module.exports = {
+	/* config options here */
+}
